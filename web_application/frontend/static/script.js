55ef0de9c8bb7bdb01c1dc0ba1c4dc4da7f4dab8
Aliaksandra Nekhviadovich <aliaksandra.nekhviadovich@rwth-aachen.de>
2022-01-10 04:13:40 +0300

    add scrollbars

--- web_application/frontend/static/script.js
+++ web_application/frontend/static/script.js
@@ -6,3 +6,13 @@ function valueCheck() {
         document.getElementById('time-periods').style.display = 'none';
    }
 }
+
+function displaymenu() {
+        if ($("li").css('display') == 'none'){
+        console.log("a");
+            $("li").css('display', 'block');
+        } else if ($("li").css('display') == 'block'){
+            $("li").css('display', 'none');
+        }
+
+}
